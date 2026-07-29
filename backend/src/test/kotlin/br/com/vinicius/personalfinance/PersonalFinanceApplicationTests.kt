16845485bbb9e7eaa386d16eae8fc61cbcece4a1
Vinicius de Oliveira Santos <56230387+vinicius-ssantos@users.noreply.github.com>
2026-07-29 15:30:13 -0300

    test: add Spring application context smoke test

---
+++ backend/src/test/kotlin/br/com/vinicius/personalfinance/PersonalFinanceApplicationTests.kt
@@ -0,0 +1,10 @@
+package br.com.vinicius.personalfinance
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PersonalFinanceApplicationTests {
+    @Test
+    fun contextLoads() = Unit
+}
