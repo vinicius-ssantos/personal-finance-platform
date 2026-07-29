a279696348d817e621cab1108141fdb950c5552f
Vinicius de Oliveira Santos <56230387+vinicius-ssantos@users.noreply.github.com>
2026-07-29 20:52:05 -0300

    style: order Testcontainers imports

--- backend/src/test/kotlin/br/com/vinicius/personalfinance/PersonalFinanceApplicationTests.kt
+++ backend/src/test/kotlin/br/com/vinicius/personalfinance/PersonalFinanceApplicationTests.kt
@@ -19,9 +19,9 @@ import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
 import org.springframework.test.context.TestConstructor
-import org.testcontainers.postgresql.PostgreSQLContainer
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
+import org.testcontainers.postgresql.PostgreSQLContainer
 import org.testcontainers.utility.DockerImageName
 
 @Testcontainers
