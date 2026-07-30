a6913b12f1dd3238553a9e88f554102b29b74d7f
Vinicius de Oliveira Santos <56230387+vinicius-ssantos@users.noreply.github.com>
2026-07-29 23:06:02 -0300

    arch: declare the backend as a Spring Modulith

--- backend/src/main/kotlin/br/com/vinicius/personalfinance/PersonalFinanceApplication.kt
+++ backend/src/main/kotlin/br/com/vinicius/personalfinance/PersonalFinanceApplication.kt
@@ -2,7 +2,12 @@ package br.com.vinicius.personalfinance
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.modulith.Modulithic
 
+@Modulithic(
+    systemName = "Personal Finance Platform",
+    sharedModules = ["shared"],
+)
 @SpringBootApplication
 class PersonalFinanceApplication
 
