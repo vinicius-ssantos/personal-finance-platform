dd47707d9c8e201600e077687323074295c0ab7d
vinicius-ssantos <viniciusolicveira@gmail.com>
2026-07-30 13:20:20 -0300

    chore: remove dead typealias, add ADR trailing newlines and version note

--- backend/src/main/kotlin/br/com/vinicius/personalfinance/database/DatabaseMigrationHealthIndicator.kt
+++
@@ -1,8 +0,0 @@
-package br.com.vinicius.personalfinance.database
-
-@Deprecated(
-    message = "Database adapters belong to the persistence module",
-    level = DeprecationLevel.HIDDEN,
-)
-internal typealias DatabaseMigrationHealthIndicator =
-    br.com.vinicius.personalfinance.persistence.health.DatabaseMigrationHealthIndicator
