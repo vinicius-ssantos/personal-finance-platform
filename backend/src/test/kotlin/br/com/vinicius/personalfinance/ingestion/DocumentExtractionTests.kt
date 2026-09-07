aba6891d37b98ecc54e862958dc30cc62e99af9f
Vinicius de Oliveira Santos <56230387+vinicius-ssantos@users.noreply.github.com>
2026-09-07 17:33:51 -0300

    style: satisfy ktlint in parser stubs

--- backend/src/test/kotlin/br/com/vinicius/personalfinance/ingestion/DocumentExtractionTests.kt
+++ backend/src/test/kotlin/br/com/vinicius/personalfinance/ingestion/DocumentExtractionTests.kt
@@ -53,8 +53,7 @@ class StubLayoutConfiguration {
             override val parserId = "test-position-parser"
             override val parserVersion = "1.4.2"
 
-            override fun parse(document: ExtractedDocument): ParsedSourceDocument =
-                object : ParsedSourceDocument {}
+            override fun parse(document: ExtractedDocument): ParsedSourceDocument = object : ParsedSourceDocument {}
         }
 }
 
