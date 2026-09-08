3e592e95a3a9dd50751dc27af71be184bbbf4e98
Vinicius de Oliveira Santos <56230387+vinicius-ssantos@users.noreply.github.com>
2026-09-07 23:34:31 -0300

    style: wrap raw snapshot descriptor identity

--- backend/src/test/kotlin/br/com/vinicius/personalfinance/ingestion/InterPositionRawSnapshot.kt
+++ backend/src/test/kotlin/br/com/vinicius/personalfinance/ingestion/InterPositionRawSnapshot.kt
@@ -5,7 +5,8 @@ internal fun renderRawSnapshot(document: InterPositionSourceDocument): String =
         appendLine(
             listOf(
                 "document",
-                "${document.descriptor.institution}/${document.descriptor.documentFamily}@${document.descriptor.layoutVersion}",
+                "${document.descriptor.institution}/${document.descriptor.documentFamily}@" +
+                    document.descriptor.layoutVersion,
                 document.parserId,
                 document.parserVersion,
                 document.declaredPositionTotalCurrencyToken ?: "<none>",
